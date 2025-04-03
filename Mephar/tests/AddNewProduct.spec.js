8e1426472d67a6aff7537fd54d93d827bf82f3d7
ThanhLan <thanhlan050109@gmail.com>
2025-04-03 13:51:56 +0700

    commit

--- Mephar/tests/AddNewProduct.spec.js
+++ Mephar/tests/AddNewProduct.spec.js
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
+import { threadId } from 'worker_threads';
 
-test('Verify that admin can create a new product when entering only required fields', 
+test('Verify that admin can create a new Merchandise when entering only required fields', 
   {
   annotation: {
-    type: "TC_GQ_01",
+    type: "TC_Create_01",
     description: `
       Precondition: 
     - The admin is logged in to the Mephar Website
@@ -14,6 +15,7 @@ test('Verify that admin can create a new product when entering only required fie
     - Product name: Thuốc đau đầu
     - Import price: 20000
     - Selling price: 25000
+    - Inventor: 1500
     - Unit: vỉ
     
       Test steps:
@@ -24,7 +26,8 @@ test('Verify that admin can create a new product when entering only required fie
     5. Observe
 
       Expected results:
-      - The system save and displays product created under 'products list' page`
+      - The system save and displays product created under 'products list' page
+      - The system create Code & Barcode`
   }
   }, async ({ page }) => {
   await page.goto('https://mephar-sit.acdtech.asia/auth/sign-in/');
@@ -45,17 +48,166 @@ test('Verify that admin can create a new product when entering only required fie
   await page.getByRole('textbox', { name: 'Nhập giá vốn' }).fill('2,0000');
   await page.getByRole('textbox', { name: 'Nhập giá bán' }).fill('2,5000');
   await page.getByRole('textbox', { name: 'Tồn kho' }).fill('2500');
+
+  const fileChooserPromise = page.waitForEvent('filechooser');
   await page.locator("//span[contains(text(),'Tải')]/ancestor::div[@class='font-semibold']/parent::div").click();
-  await page.setInputFiles("//input[@type='file']", 'D:/Playwright/Auto_Playwright/Mephar/img/anh2.jpg');
+  const fileChooser = await fileChooserPromise;
+  // Chọn ảnh (sẽ tự động đóng hộp thoại)
+  await fileChooser.setFiles('D:/Playwright/Auto_Playwright/Mephar/img/anh2.jpg');
+  await page.waitForTimeout(1000);
+
   await page.getByRole('textbox', { name: 'Nhập đơn vị cơ bản' }).fill('vỉ');
 
   //4. Click 'Luu' button.
   await page.getByRole('button', { name: 'Lưu', exact: true }).click();
   
   //5.Observe
+  await expect(page.locator("//table//tr[2]/td[3]")).toBeVisible();
   await expect(page.locator('tbody')).toContainText('2,500');
   await expect(page.locator('tbody')).toContainText('25,000đ');
   await expect(page.locator('tbody')).toContainText('20,000đ');
 });
 
+test('Verify that admin can create a new Merchandise when entering All fields', 
+  {
+  annotation: {
+    type: "TC_Create_02",
+    description: `
+      Precondition: 
+    - The admin is logged in to the Mephar Website
+      
+      Test data: 
+    - User Name: test30
+    - Password: 12345678
+    - Code: HH00002
+    - BarCode: HH00002
+    - Product name: TThuốc dạ dày
+    - Summary name: thuốc
+    - Gruop: N003
+    - Location: TT2
+    - Import price: 20000
+    - Selling price: 25000
+    - Weight: 100
+    - Inventor: 200
+    - Unit: vỉ
+    
+      Test steps:
+    1. Log into the Mephar system.
+    2. Navigate to 'add-package'.
+    3. In the add-package section, enter all fields
+    4. Click 'Luu' button.
+    5. Observe
 
+      Expected results:
+      - The system save and displays product created under 'products list' page`
+  }
+  }, async ({ page }) => {
+  await page.goto('https://mephar-sit.acdtech.asia/auth/sign-in/');
+
+  //1.The admin is logged in to the Mephar Website
+  await page.getByRole('textbox', { name: 'Tên đăng nhập' }).fill('test30');
+  await page.getByRole('textbox', { name: 'Mật khẩu' }).fill('12345678');
+  await page.getByRole('button', { name: 'Đăng nhập' }).click();
+
+  //2.Navigate to 'add-package'
+  await page.getByRole('menuitem', { name: 'Sản phẩm' }).click();
+  await page.getByRole('menuitem', { name: 'Danh sách sản phẩm' }).click();
+  await page.getByRole('button', { name: 'Thêm mới' }).click();
+  await page.getByText('Thêm mới hàng hóa').click();
+
+  //3.Enter all fields.
+  await page.getByRole('textbox', { name: 'Mã hàng tự động' }).fill('HH00005');
+  await page.getByRole('textbox', { name: 'Mã vạch tự động' }).fill('HH00004');
+  await page.getByRole('textbox', { name: 'Nhập tên hàng hóa' }).fill('Thuốc dạ dày');
+  await page.getByRole('textbox', { name: 'Nhập tên viết tắt' }).fill('thuốc ');
+  await page.locator("//span[contains(text(),'Nhóm sản phẩm')]/parent::div").click();
+  await page.getByText('N003').click();
+  await page.locator("//span[contains(text(),'Vị trí sản phẩm')]/parent::div").click();
+  await page.getByText('TT2').click();
+  await page.getByRole('textbox', { name: 'Nhập giá vốn' }).fill('2,0000');
+  await page.getByRole('textbox', { name: 'Nhập giá bán' }).fill('2,5000');
+  await page.getByRole('textbox', { name: 'Nhập trọng lượng' }).fill('100');
+  await page.getByRole('textbox', { name: 'Quy cách đóng gói' }).fill('hộp');
+  await page.locator("//span[contains(text(),'Nhập tên hãng')]/parent::div").click();
+  await page.getByTitle('Bilim Ilac Sanayi Ve Ticaret').click();
+  await page.locator("//span[contains(text(),'Nhập tên nước')]/parent::div").click();
+  await page.getByText('Litva').click();
+  await page.getByRole('textbox', { name: 'Tồn kho' }).fill('200');
+
+  const fileChooserPromise = page.waitForEvent('filechooser');
+  await page.locator("//span[contains(text(),'Tải')]/ancestor::div[@class='font-semibold']/parent::div").click();
+  const fileChooser = await fileChooserPromise;
+  // Chọn ảnh (sẽ tự động đóng hộp thoại)
+  await fileChooser.setFiles('D:/Playwright/Auto_Playwright/Mephar/img/anh2.jpg');
+  await page.waitForTimeout(1000);  
+  
+  await page.getByRole('textbox', { name: 'Nhập đơn vị cơ bản' }).fill('vỉ');
+  //4. Click 'Luu' button.
+  await page.getByRole('button', { name: 'Lưu', exact: true }).click();
+  
+  //5.Observe
+  await expect(page.locator('//table//tr[2]/td[7]')).toContainText('200');
+  await expect(page.locator('//table//tr[2]/td[9]')).toContainText('25,000đ');
+  await expect(page.locator('//table//tr[2]/td[10]')).toContainText('20,000đ');
+});
+
+test('Verify that admin can not create a new Merchandise when Code&Barcode existed', 
+  {
+  annotation: {
+    type: "TC_Create_03",
+    description: `
+      Precondition: 
+    - The admin is logged in to the Mephar Website
+      
+      Test data: 
+    - User Name: test30
+    - Password: 12345678
+    - Product name: TThuốc dạ dày
+    - Summary name: thuốc
+    - Gruop: N003
+    - Location: TT2
+    - Import price: 20000
+    - Selling price: 25000
+    - Weight: 100
+    - Inventor: 200
+    - Unit: vỉ
+    
+      Test steps:
+    1. Log into the Mephar system.
+    2. Navigate to 'add-package'.
+    3. In the add-package section, enter Code & Barcode existed and required fields
+    4. Click 'Luu' button.
+    5. Observe
+
+      Expected results:
+      - The system can not creat a product under 'products list' page
+      - The system display erorr messaage`
+  }
+  }, async ({ page }) => {
+  await page.goto('https://mephar-sit.acdtech.asia/auth/sign-in/');
+
+  //1.The admin is logged in to the Mephar Website
+  await page.getByRole('textbox', { name: 'Tên đăng nhập' }).fill('test30');
+  await page.getByRole('textbox', { name: 'Mật khẩu' }).fill('12345678');
+  await page.getByRole('button', { name: 'Đăng nhập' }).click();
+
+  //2.Navigate to 'add-package'
+  await page.getByRole('menuitem', { name: 'Sản phẩm' }).click();
+  await page.getByRole('menuitem', { name: 'Danh sách sản phẩm' }).click();
+  await page.getByRole('button', { name: 'Thêm mới' }).click();
+  await page.getByText('Thêm mới hàng hóa').click();
+
+  //3.Enter all fields.
+  await page.getByRole('textbox', { name: 'Mã hàng tự động' }).fill('HH00002');
+  await page.getByRole('textbox', { name: 'Mã vạch tự động' }).fill('HH00002');
+  await page.getByRole('textbox', { name: 'Nhập tên hàng hóa' }).fill('Thuốc đau đầu');
+  await page.getByRole('textbox', { name: 'Nhập giá vốn' }).fill('2,0000');
+  await page.getByRole('textbox', { name: 'Nhập giá bán' }).fill('2,5000');
+  await page.getByRole('textbox', { name: 'Tồn kho' }).fill('2500');
+  await page.getByRole('textbox', { name: 'Nhập đơn vị cơ bản' }).fill('vỉ');
+  //4. Click 'Luu' button.
+  await page.getByRole('button', { name: 'Lưu', exact: true }).click();
+  
+  //5.Observe
+  await expect(page.locator('body')).toContainText('Mã hàng HH00002 đã tồn tại.');
+});
--- Mephar/tests/Delete.spec.js
+++ Mephar/tests/Delete.spec.js
@@ -1,15 +1,103 @@
 import { test, expect } from '@playwright/test';
+import { time } from 'console';
 
-test('test', async ({ page }) => {
+test('Verify that admin can delete a product successfully', 
+  {
+  annotation: {
+    type: "TC_Delete_01",
+    description: `
+      Precondition: 
+    - The admin is logged in to the Mephar Website
+      
+      Test data: 
+    - Product name: Thuốc đau đầu
+    
+      Test steps:
+    1. Log into the Mephar system.
+    2. Navigate to 'products list'.
+    3. In the products list section, choose a product to delete.
+    4. Click 'Xoa' button.
+    5. Confirm delete--> click "Xoa" button
+    6. Observe
+
+      Expected results:
+      - The system remover deleted product from 'products list' page`
+  }
+  }, async ({ page }) => {
   await page.goto('https://mephar-sit.acdtech.asia/auth/sign-in/');
-  await page.getByRole('textbox', { name: 'Tên đăng nhập' }).click();
+  const PRODUCT_ID = 'HH000000032'; // Thay bằng ID của sản phẩm cần xóa
+
+//1. Log into the Mephar system.
   await page.getByRole('textbox', { name: 'Tên đăng nhập' }).fill('test30');
-  await page.getByRole('textbox', { name: 'Mật khẩu' }).click();
   await page.getByRole('textbox', { name: 'Mật khẩu' }).fill('12345678');
   await page.getByRole('button', { name: 'Đăng nhập' }).click();
+//2. Navigate to 'products list'.
   await page.getByRole('menuitem', { name: 'Sản phẩm' }).click();
   await page.getByRole('menuitem', { name: 'Danh sách sản phẩm' }).click();
-  await page.locator('.ant-table-row > td:nth-child(4)').first().click();
+  // Xác minh sản phẩm tồn tại trước khi xóa
+  const productLocator = page.locator(`//td[contains(text(),'${PRODUCT_ID}')]`);
+  await expect(productLocator).toBeVisible();
+
+//3. In the products list section, choose a product to delete.
+  await productLocator.click();
+//4. Click 'Xoa' button.
   await page.getByRole('button', { name: 'Xoá' }).click();
-  await page.getByRole('button', { name: 'Xóa' }).click();
+//5. Confirm delete--> click "Xoa" button
+const confirmButton= page.getByRole('button', { name: 'Xóa' });
+if(await confirmButton.isVisible()){
+  await confirmButton.click();
+}
+await expect(productLocator).not.toBeVisible();
+});
+
+
+test('verify that admin cannot delete product when product has order', 
+  {
+  annotation: {
+    type: "TC_Delete_02",
+    description: `
+      Precondition: 
+    - The admin is logged in to the Mephar Website
+      
+      Test data: 
+    - Product name: Thuốc đau đầu
+    
+      Test steps:
+    1. Log into the Mephar system.
+    2. Navigate to 'products list'.
+    3. In the products list section, choose a product to delete.
+    4. Click 'Xoa' button.
+    5. Confirm delete--> click "Xoa" button
+    6. Observe
+
+      Expected results:
+      - admin cannot delete product when product has order`
+  }
+  }, async ({ page }) => {
+    
+  await page.goto('https://mephar-sit.acdtech.asia/auth/sign-in/');
+  const PRODUCT_ID = 'HH000000001'; // Thay bằng ID của sản phẩm cần xóa
+
+//1. Log into the Mephar system.
+  await page.getByRole('textbox', { name: 'Tên đăng nhập' }).fill('test30');
+  await page.getByRole('textbox', { name: 'Mật khẩu' }).fill('12345678');
+  await page.getByRole('button', { name: 'Đăng nhập' }).click();
+//2. Navigate to 'products list'.
+  await page.getByRole('menuitem', { name: 'Sản phẩm' }).click();
+  await page.getByRole('menuitem', { name: 'Danh sách sản phẩm' }).click();
+  // Xác minh sản phẩm tồn tại trước khi xóa
+  const productLocator = page.locator(`//td[contains(text(),'${PRODUCT_ID}')]`);
+  await expect(productLocator).toBeVisible();
+
+//3. In the products list section, choose a product to delete.
+  await productLocator.click();
+//4. Click 'Xoa' button.
+  await page.getByRole('button', { name: 'Xoá' }).click();
+//5. Confirm delete--> click "Xoa" button
+  const confirmButton= page.getByRole('button', { name: 'Xóa' });
+  if(await confirmButton.isVisible()){
+    await confirmButton.click();
+  }
+  await expect(page.locator('body')).toContainText('Sản phẩm đang được sử dụng ở nhập hàng, không thể xóa');
+  
 });
